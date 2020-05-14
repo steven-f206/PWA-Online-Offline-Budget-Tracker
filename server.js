82864ffbe5b587d96a66eefb9e457fce55e278b4
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2020-05-14 14:10:19 -0400

    Checking for module error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv/config');
 const connectDB = require("./config/connectDB.js");
 
 //Bring in models
-const db = require("./models");
+// const db = require("./models");
 const app = express();
 const PORT = process.event.PORT || 3000;
 
@@ -20,53 +20,53 @@ app.use(express.static("public"));
 /******************************* MiddleWare  ****************************/
 
 
-//GET REQUESTS
+// //GET REQUESTS
 
-app.get("/api/transaction", (req, res) => {
-  db.Transaction.find({})
-    .then(dbData => {
-      res.json(dbData);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+// app.get("/api/transaction", (req, res) => {
+//   db.Transaction.find({})
+//     .then(dbData => {
+//       res.json(dbData);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
-app.post("/api/transaction", (req, res) => {
-  let data = req.body;
-  console.log(data);
+// app.post("/api/transaction", (req, res) => {
+//   let data = req.body;
+//   console.log(data);
 
-  db.Transaction.create({
-    date: data.date,
-    name: data.name,
-    value: data.value
-  }).then(dbUpdate => {
-    res.json(dbUpdate);
-  })
-    .catch(err => {
-      res.json(err);
-    });
-});
+//   db.Transaction.create({
+//     date: data.date,
+//     name: data.name,
+//     value: data.value
+//   }).then(dbUpdate => {
+//     res.json(dbUpdate);
+//   })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
-app.post("/api/transaction/bulk", (req, res) => {
-  let data = req.body;
-  console.log(data);
+// app.post("/api/transaction/bulk", (req, res) => {
+//   let data = req.body;
+//   console.log(data);
 
-  db.Transaction.create({
-    date: data.date,
-    name: data.name,
-    value: data.value
-  }).then(dbUpdate => {
-    res.json(dbUpdate);
-  })
-    .catch(err => {
-      res.json(err);
-    });
-});
+//   db.Transaction.create({
+//     date: data.date,
+//     name: data.name,
+//     value: data.value
+//   }).then(dbUpdate => {
+//     res.json(dbUpdate);
+//   })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
 
 
 /******************************* Connect to db  ****************************/
-connectDB()
+// connectDB()
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
